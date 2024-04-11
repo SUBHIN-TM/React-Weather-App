1de0199088ee7e2d3abf329ba1d2ce5c1b51f18f
SUBHIN-TM <subhintm2018@gmail.com>
2024-04-11 11:04:26 +0530

    sort done now impliment infinite scroll

--- src/Comopnents/Table.jsx
+++ src/Comopnents/Table.jsx
@@ -6,15 +6,49 @@ import { FaSort } from "react-icons/fa6";
 
 const Table = () => {
     const [city, setcity] = useState("")
+    const [sort,setsort]=useState("")
     useEffect(() => {
         allCity()
-    }, [])
+    }, [sort])
+
+    let sortFuction=(parameter)=>{
+     if(parameter === 'name'){
+        setsort((previous)=>{
+            if (previous=== 'name'){
+                return '-name'
+            }else{
+                return 'name'
+            }
+        })
+     }else if(parameter === 'cou_name_en'){
+        setsort((previous)=>{
+            if (previous=== 'cou_name_en'){
+                return '-cou_name_en'
+            }else{
+                return 'cou_name_en'
+            }
+        })
+     }else if(parameter === 'timezone'){
+        setsort((previous)=>{
+            if (previous=== 'timezone'){
+                return '-timezone'
+            }else{
+                return 'timezone'
+            }
+        })
+     }
+    }
 
 
 
     const allCity = async () => {
         try {
-            const response = await axios.get('https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-500/records?limit=100')
+            let response
+            if(sort){
+                response = await axios.get(`https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?order_by=${sort}&limit=100&offset=0`)
+            }else{
+                response = await axios.get('https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=100')
+            }     
             setcity(response.data.results)
         } catch (error) {
             console.error(error);
@@ -32,12 +66,12 @@ const Table = () => {
                     <input className="ml-5" type="search" name="city" id="" placeholder="City Name" />
                 </div>
                 <div className="tableContainer p-3">
-                    <table className="overflow-y-scroll inline-block  w-12/12 max-h-[650px]">
+                    <table className="overflow-y-scroll inline-block  max-h-[650px]">
                         <thead className="sticky top-0 bg-black text-white">
                             <tr className="">
-                                <th  className="w-6/12 text-start p-3"><span>City Name</span><span className="inline-block text-lg pl-3"><FaSort /></span></th>
-                                <th className="w-4/12 text-start"><span>Country</span><span className="inline-block  pl-3"><FaSort /></span></th>
-                                <th className="w-2/12 text-start"><span>TimeZone</span><span className="inline-block  pl-3"><FaSort /></span></th>
+                                <th  className="w-6/12 text-start p-3"><span>City Name</span><span onClick={()=> sortFuction('name')} className="inline-block text-lg pl-3"><FaSort /></span></th>
+                                <th className="w-4/12 text-start"><span>Country</span><span onClick={()=> sortFuction('cou_name_en')} className="inline-block  pl-3"><FaSort /></span></th>
+                                <th className="w-2/12 text-start"><span>TimeZone</span><span onClick={()=> sortFuction('timezone')} className="inline-block  pl-3"><FaSort /></span></th>
                             </tr>
                         </thead>
                       
--- src/Comopnents/TableBody.jsx
+++ src/Comopnents/TableBody.jsx
@@ -4,7 +4,7 @@ const TableBody = ({city}) => {
     return (
         <tr id="tableRow" className="font-semibold text-yellow-200" >
          <td className="pl-3 pb-1 p-2">{city.name}</td>
-         <td>{city.country}</td>
+         <td>{city.cou_name_en}</td>
          <td>{city.timezone}</td>
         </tr>
     )
