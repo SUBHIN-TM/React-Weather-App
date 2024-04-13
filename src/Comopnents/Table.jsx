6e635cdfcbee03cf19d2ff3b02e8cf5a4f3412d6
SUBHIN-TM <subhintm2018@gmail.com>
2024-04-13 18:07:05 +0530

    wether api should integreate

--- src/Comopnents/Table.jsx
+++ src/Comopnents/Table.jsx
@@ -3,39 +3,38 @@ import axios from 'axios'
 import TableBody from "./TableBody"
 import { FaSort } from "react-icons/fa6";
 import Filter from "./Filter";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Table = () => {
     const [city, setcity] = useState([])
     const [sort, setsort] = useState("")
     const [offset, setoffset] = useState(0)
-    const [selectedcountries,setselectedcountries]=useState([])
-    const [selectedtimezone,setselectedtimezone]=useState([])
-    const [search,setsearch]=useState("")
-    const [isLoading,setIsloading]=useState(false)
-    const [error,setError]=useState(null)
+    const [selectedcountries, setselectedcountries] = useState([])
+    const [selectedtimezone, setselectedtimezone] = useState([])
+    const [search, setsearch] = useState("")
+    const [isLoading, setIsloading] = useState(false)
+    const [error, setError] = useState(null)
     const tableRef = useRef(null) //REFFERENCE FOR SCROLLABLE CONTROLLER
-   
-    let baseURL=`https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20&offset=${offset}`
+
+    let baseURL = `https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=20&offset=${offset}`
 
     useEffect(() => {
         allCity()
-    }, [sort,selectedcountries,selectedtimezone,search])
+    }, [sort, selectedcountries, selectedtimezone, search])
 
 
-    const selectedCountryLiftUp=(selectedCountries)=>{ 
-        setselectedcountries(selectedCountries) 
+    const selectedCountryLiftUp = (selectedCountries) => {
+        setselectedcountries(selectedCountries)
         setoffset(0)
-        setcity([])   
+        setcity([])
     }
-   
-    const selectedTimeLiftUp=(selectedTime)=>{ 
+
+    const selectedTimeLiftUp = (selectedTime) => {
         setselectedtimezone(selectedTime)
         setoffset(0)
-        setcity([])       
+        setcity([])
     }
-
-    
-    console.log("STATUS",isLoading);
 
 
 
@@ -72,57 +71,58 @@ const Table = () => {
     }
 
     const allCity = async () => {
-        console.log("api get called");
         try {
-            let finalURL=baseURL
-            if(sort){
-                finalURL +=`&order_by=${sort}`
+            let finalURL = baseURL
+            if (sort) {
+                finalURL += `&order_by=${sort}`
             }
-            if(selectedcountries.length >0 ){
+            if (selectedcountries.length > 0) {
                 const filterCountryArrayJoinedString = selectedcountries.map(country => `&refine=cou_name_en:"${country}"`).join('');
-                finalURL +=`${filterCountryArrayJoinedString}`         
+                finalURL += `${filterCountryArrayJoinedString}`
             }
-            if(selectedtimezone.length >0){
-                const filterTimeZoneArrayJoinedString=selectedtimezone.map(time => `&refine=timezone:"${time}"`).join('');
-                finalURL +=`${filterTimeZoneArrayJoinedString}`   
+            if (selectedtimezone.length > 0) {
+                const filterTimeZoneArrayJoinedString = selectedtimezone.map(time => `&refine=timezone:"${time}"`).join('');
+                finalURL += `${filterTimeZoneArrayJoinedString}`
             }
-            if(search){ 
-                finalURL +=`&where="${search}"`
+            if (search) {
+                finalURL += `&where="${search}"`
             }
-            console.log("Final URL",finalURL);
-             setIsloading(true)
-            const response=await axios.get(finalURL)  
-                setcity((previous) => [...previous, ...response.data.results])
-                setoffset((pvs) => pvs + 20) 
-        
+            console.log("Final URL", finalURL);
+            setIsloading(true)
+            const response = await axios.get(finalURL)
+            setcity((previous) => [...previous, ...response.data.results])
+            setoffset((pvs) => pvs + 20)
+
         } catch (error) {
             console.error(error);
+            toast.error("Something Went Wrong")
             setError(error)
         }
-        finally{
+        finally {
             setIsloading(false)
         }
     }
 
     const handleScroll = () => {
         const { scrollTop, offsetHeight, scrollHeight } = tableRef.current; //THIS DIV CURRENT PROPETIES WILL GIVE SCROLL VALUES ,SO DESTRUCTURE IT AND USE ALTERNATIVELY
-       //scrollTop=no of px scrolled vertically form the messure from top , offsetHeight=fixed heigtht of element or  , scrollheight=total height of content including overflow
-    //    console.log("top",scrollTop); //
-    //    console.log("off",offsetHeight);
-    //    console.log("scr hit",scrollHeight);
-       if (scrollTop+2 + offsetHeight >= scrollHeight) {
+        //scrollTop=no of px scrolled vertically form the messure from top , offsetHeight=fixed heigtht of element or  , scrollheight=total height of content including overflow
+        if (scrollTop + 2 + offsetHeight >= scrollHeight) {
             allCity()
         }
     }
 
-    const searchFunction=(value)=>{
-      setsearch(value)
-      setoffset(0)
-      setcity([])
+    const searchFunction = (value) => {
+        setsearch(value)
+        setoffset(0)
+        setcity([])
     }
 
     if (!city) {
-        return
+        return(
+            <>
+            <div id="loader"></div>
+            </>
+        )
     }
 
     return (
@@ -130,10 +130,10 @@ const Table = () => {
             <div className="flex flex-col gap-5   px-24">
                 <div id="searchDiv" >
                     <label className=" font-semibold " htmlFor="">SEARCH</label>
-                    <input onChange={(e)=>searchFunction(e.target.value)} value={search} className="px-3 py-1 ml-6 rounded-lg" type="search" name="city" id="" placeholder="City Name" />
+                    <input onChange={(e) => searchFunction(e.target.value)} value={search} className="px-3 py-1 ml-6 rounded-lg" type="search" name="city" id="" placeholder="City Name" />
                 </div>
                 <div className="block lg:flex lg:gap-6">
-                    <div className="tableContainer  overflow-y-auto  lg:w-7/12" style={{ maxHeight: 599}} ref={tableRef} onScroll={handleScroll}>
+                    <div className="tableContainer  overflow-y-auto  lg:w-7/12" style={{ maxHeight: 599 }} ref={tableRef} onScroll={handleScroll}>
                         <table className=" w-full">
                             <thead className=" bg-black text-white text-left sticky top-0">
                                 <tr className="">
@@ -142,23 +142,23 @@ const Table = () => {
                                     <th className="p-3 w-3/12"><span>TimeZone</span><span onClick={() => sortFuction('timezone')} className="inline-block  pl-3"><FaSort /></span></th>
                                 </tr>
                             </thead>
-
+                           
                             <tbody >
-                                {city.map((cities,index) => {
-                                    return <TableBody key={cities.geoname_id+index} city={cities} />
+                                {city.map((cities, index) => { //*CREATED A SEPERATE COMPONENT FOR DISPLAYING EACH TABLE ROWS
+                                    return <TableBody key={cities.geoname_id + index} city={cities} />
                                 })}
-
                             </tbody>
                         </table>
+                        {/* LOADING IS ONLY TRUE WHEN THE FETCH CALLING IS WAITING */}
                         {isLoading && <h1 id="loading" className="text-white font-semibold p-3" >Loading...</h1>}
-                      
                     </div>
-
+                      {/* CREATED ANOTHER COMPONENT FOR FILTER SECTION */}
                     <Filter selectedCountryLiftUp={selectedCountryLiftUp} selectedcountries={selectedcountries} selectedTimeLiftUp={selectedTimeLiftUp} selectedtimezone={selectedtimezone} />
-
                 </div>
-
-            </div >
+            </div>
+            <div>
+                <ToastContainer />
+            </div>
         </>
     )
 }
